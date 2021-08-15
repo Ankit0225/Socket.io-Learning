e64c9868f11ee92c0edfd49582cbd8087dd33465
Ankit <ankitmaurya0225@gmail.com>
2021-08-15 17:40:58 +0530

    server.js

--- Socket Intro/server.js
+++ Socket Intro/server.js
@@ -5,7 +5,7 @@ const app = express()
 const server = http.createServer(app)
 app.use(express.static(__dirname + '/public'))
 
-app.listen('4003', () => {
+server.listen('4003', () => {
     console.log('Server Started at http://localhost:4003');
 })
 
