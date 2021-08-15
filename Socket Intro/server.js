062775705e95e3d7a9fdb05c51b5c62918a09436
Ankit <ankitmaurya0225@gmail.com>
2021-08-15 17:19:51 +0530

    server.js

--- Socket Intro/server.js
+++ Socket Intro/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const app = express()
 
+const server = http.createServer(app)
 app.use(express.static(__dirname + '/public'))
 
 app.listen('4003', () => {
