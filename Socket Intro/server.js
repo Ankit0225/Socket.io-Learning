2cf9cccba2b23ef71408fb417e597c11e9aefd1d
Ankit <ankitmaurya0225@gmail.com>
2021-08-15 20:14:38 +0530

    added socket.io

--- Socket Intro/server.js
+++ Socket Intro/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const http = require('http')
-const app = express()
+const socketio = require('socket.io')
 
+const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
+
 app.use(express.static(__dirname + '/public'))
 
 server.listen('4003', () => {
