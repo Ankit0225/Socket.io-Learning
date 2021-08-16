0681f15d7a002c1a7518e5f8414e6ed9fdeac556
Ankit <ankitmaurya0225@gmail.com>
2021-08-16 21:56:05 +0530

    public/script.js

--- Color Project With Sockets/public/script.js
+++ Color Project With Sockets/public/script.js
@@ -3,3 +3,15 @@ let socket = io()
 socket.on('connect',() => {
     document.getElementById('socketid').innerText = socket.id
 })
+
+function colorthebox(color) {
+    document.querySelector("."+color).style.backgroundColor = color
+    setTimeout(() => {
+        document.querySelector("."+color).style.backgroundColor = null
+    }, 500)
+}
+
+document.getElementById('Colorit').onclick = function () {
+ const color = document.getElementById('selectedcolor').value
+ colorthebox(color)  
+}
