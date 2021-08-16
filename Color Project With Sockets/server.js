a234847779118ae6eab0dd197802059ce6501633
Ankit <ankitmaurya0225@gmail.com>
2021-08-16 17:24:32 +0530

    modified server.js

---
+++ Color Project With Sockets/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+
+
+const app = express()
+const server =  http.createServer(app)
+const io = socketio(server)
+
+
+app.use(express.static(__dirname + '/public'))
+
+io.on('connection',(socket) => {
+    console.log('Connected to =', socket.id);
+})
+
+server.listen('5000',() => {
+ console.log('Server Started on http://localhost:5000');
+})
