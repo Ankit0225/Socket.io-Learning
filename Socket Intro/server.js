f3f29eab77b2b637c19871889cf7fe7b008b3831
Ankit <ankitmaurya0225@gmail.com>
2021-08-16 15:12:10 +0530

    server.js

--- Socket Intro/server.js
+++ Socket Intro/server.js
@@ -8,6 +8,10 @@ const io = socketio(server)
 
 app.use(express.static(__dirname + '/public'))
 
+io.on('connection', (socket) => {
+    console.log('connected to =' ,socket.id);
+})
+
 server.listen('4003', () => {
     console.log('Server Started at http://localhost:4003');
 })
